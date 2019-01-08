777953422276775f355a7c0696ab5456fac84f3c
Noel Vock <noel1uk@gmail.com>
2019-01-08 20:47:09 +0000

    example of added value and a secondary endpoint and path variable

--- src/main/java/com/intro/returnjson/controllers/ExampleController.java
+++ src/main/java/com/intro/returnjson/controllers/ExampleController.java
@@ -1,7 +1,7 @@
 package com.intro.returnjson.controllers;
 
 import com.intro.returnjson.model.ExampleObject;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="some-json")
 public class ExampleController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "mu", method = RequestMethod.GET)
     public ExampleObject getExample() {
         return new ExampleObject()
                 .withId("1")
                 .withOrganisation("foo")
                 .withPrice("£1");
+    }
+
+    @RequestMapping(value="{id}", method = RequestMethod.GET)
+    public ExampleObject getPathVariables(
+            @PathVariable("id") String id) {
+        return new ExampleObject()
+                .withId(id)
+                .withOrganisation("soo")
+                .withPrice("£10");
     }
 
 
